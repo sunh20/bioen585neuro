6f4bb64df99ee8585dc5028632bfe9779cb89707
SamSun <sunh20@uw.edu>
2019-05-08 11:51:25 -0700

    got simple model working

--- code/simpleIZ.m
+++ code/simpleIZ.m
@@ -2,19 +2,26 @@ function dydt = simpleIZ(t,y,params)
 
 v = y(1);
 u = y(2);
-I = params(1);
-a = params(2);
-b = params(3);
-c = params(4);
-d = params(5);
-maxSpike = params(6);
+a = params{1};
+b = params{2};
+c = params{3};
+d = params{4};
+maxSpike = params{5};
+tspan = params{6};
+I = params{7};
+
+dydt = zeros(2,1);
 
 if v >= maxSpike
+    disp('Max spike reached')
     v = c;
     u = u + d;
 end
-    
-dydt(1) = 0.04*v^2 + 5*v + 140 - u + I;
+
+% get input value at this time
+I_t = interp1(tspan,I,t);
+
+dydt(1) = 0.04*v^2 + 5*v + 140 - u + I_t;
 dydt(2) = a * (b*v - u); 
 
 end
--- code/starterCode.m
+++ code/starterCode.m
@@ -6,22 +6,62 @@ clear all; close all; clc;
 
 % izhikevich simple model
 
+% model coefficients
+% from paper: 0.04*v^2 + 5*v + 140
+% can change this to the format in the book
+A = 0.04;
+B = 5;
+C = 140;
+
 % parameters
 a = 0.02;   % time scale of recovery u
-b = 0.2;    % sensitivity of recovery
+b = 2;    % sensitivity of recovery
 c = -65;    % mV, post spike reset for v
 d = 8;      % post spike reset for u
 maxSpike = 30; % threshold potential
-I = 0;      % input 
-params = [I, a, b, c, d, maxSpike];
 
-% variables - initial conditions
+% variables
+dt = 0.01;
+t = 0:dt:100; % time span
+I = zeros(length(t),1);
+I(500:500:end) = 40;  % +40 mV input every 5 seconds
 
-v_0 = c;
-u_0 = d;
-y = [v_0,u_0];
+u = zeros(length(t),1);
+v = zeros(length(t),1);
+u(1) = d;   % ICs
+v(1) = c;   % ICs
 
-% ode
+% ode - manually step through using Forward Euler
+for idx = 1:length(t)-1
+    % if reaches max spike potential
+    if v(idx) >= maxSpike
+        disp('Max spike reached')
+        v(idx) = maxSpike; % makes sure spikes don't exceed this
+        v(idx+1) = c;
+        u(idx+1) = u(idx) + d;
+    else      
+        % ODE: next = current + dt * dy/dt
+        u(idx+1) = u(idx) + dt * (a * (b*v(idx) - u(idx)));
+        v(idx+1) = v(idx) + dt * (A*v(idx)^2 + B*v(idx) + C - u(idx) + I(idx));
+    end
+end
+
+% ode - using ode solver (doesn't work because you can't assign variables
+%                           directly i.e. when v -> -65, can potentially
+%                           be implemented using Events options
+% [t,y] = ode45(@simpleIZ,tspan,IC,[],params);
+
+% plot time repsonse vs. input
+figure;
+
+subplot(2,1,1)
+plot(t,v)
+title('Voltage time response of Izhikevich simple model')
+
+subplot(2,1,2)
+plot(t,I)
+title('Input function')
+
 
 
 
