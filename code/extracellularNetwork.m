d8532a2443c3eb3cb6b9893a00e2729c4aa205c5
kelseyluu <kelseytl@uw.edu>
2019-05-30 11:32:24 -0700

    2 neuron network

--- code/extracellularNetwork.m
+++ code/extracellularNetwork.m
@@ -3,15 +3,16 @@ load('neuron2.mat')
 global data 
 data = avg_NEURON(26:175);
 max_t = 2;
+dt = 1e-5;
 tspan = 0:dt:max_t;
 fire_1 = randi([0 1],size(tspan));
 fire_2 = zeros(1,length(tspan));
-dt = 1e-5;
+
 maxSpike = 30;
 rest = -65;
 extra = zeros(1,length(fire_1));
-for i = 1:(length(fire)-length(data))
-    if fire(i) == 1
+for i = 1:(length(fire_1)-length(data))
+    if fire_1(i) == 1
         for j = 1:length(data)
             k = i + j - 1;
             extra(k) = extra(k) + data(j);
