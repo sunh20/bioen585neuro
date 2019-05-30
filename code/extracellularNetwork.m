e96c442bc56df5aa585272bd6ca6f51071249710
kelseyluu <kelseytl@uw.edu>
2019-05-30 16:15:00 -0700

    updated extracellular potential model with random firing input

--- code/extracellularNetwork.m
+++ code/extracellularNetwork.m
@@ -1,35 +1,32 @@
-close all; clc;
+close all; clear all; clc;
 load('neuron2.mat')
-global data 
+global data
 data = avg_NEURON(26:175);
-max_t = 2;
-dt = 1e-5;
-tspan = 0:dt:max_t;
-fire_1 = randi([0 1],size(tspan));
-fire_2 = zeros(1,length(tspan));
 
+dt = 1e-5;  %time step
+max_t = 2;  %max time
+tspan = 0:dt:max_t;
 maxSpike = 30;
 rest = -65;
-extra = zeros(1,length(fire_1));
-for i = 1:(length(fire_1)-length(data))
-    if fire_1(i) == 1
-        for j = 1:length(data)
-            k = i + j - 1;
-            extra(k) = extra(k) + data(j);
-        end 
 
-        if extra(i) >= maxSpike
-            fire_2(i) = 1;
+
+% binary firing inputs for each neuron:
+neuronCount = 5;
+fire = randi([0 1], neuronCount, length(tspan));  % each neuron = 1 row
+[r, c] = size(fire);
+
+% summed extracellular potential:
+ec = zeros(r, c);
+for neuronInd = 1:r
+    for i = 1:(c - length(data))
+        if fire(neuronInd, i) == 1
             for j = 1:length(data)
                 k = i + j - 1;
-                extra(k) = extra(k) + data(j);
-            end 
+                ec(neuronInd, k) = ec(neuronInd, k) + data(j);
+            end
         end
     end
-%     if extra(i) >= maxSpike
-%         extra(i) = maxSpike;
-%         extra(i+1) = rest;
-%     end
+    subplot(neuronCount, 1, neuronInd);
+    plot (tspan,ec(neuronInd,:));  
 end
 
-plot(tspan,extra)
