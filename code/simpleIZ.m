0d1d4d4ed3f8a115cf201096c172a1d86fd6d911
Samantha Sun <sunh20@uw.edu>
2019-06-02 15:53:13 -0700

    adding figures + figure code

--- code/simpleIZ.m
+++ code/simpleIZ.m
@@ -1,27 +1,70 @@
-function dydt = simpleIZ(t,y,params)
+function simpleIZ(t,dt,input)
 
-v = y(1);
-u = y(2);
-a = params{1};
-b = params{2};
-c = params{3};
-d = params{4};
-maxSpike = params{5};
-tspan = params{6};
-I = params{7};
+% model coefficients
+% from paper: 0.04*v^2 + 5*v + 140
+% can change this to the format in the book
+A = 0.04;
+B = 5;
+C = 140;
 
-dydt = zeros(2,1);
+% parameters
+a = 0.02;   % time scale of recovery u
+b = 0.2;    % sensitivity of recovery
+c = -65;    % mV, post spike reset for v
+d = 8;      % post spike reset for u
+maxSpike = 30; % max spike potential
 
-if v >= maxSpike
-    disp('Max spike reached')
-    v = c;
-    u = u + d;
+% other params (for function default)
+% dt = 0.01;
+% t = 0:dt:100; % time span (units?)
+% I = zeros(length(t),1);
+% 
+% % input 1: +40 mV square pulses 
+% I(500:1000) = 40;  % +40 mV square pulse
+% I(2500:3000) = 40;  % +40 mV square pulse
+% I(4500:5000) = 40;  % +40 mV square pulse
+% I(6500:7000) = 40;  % +40 mV square pulse
+% I(8500:9000) = 40;  % +40 mV square pulse
+
+% input
+I = input;
+
+% variables
+u = zeros(length(t),1);
+v = zeros(length(t),1);
+u(1) = d;   % ICs
+v(1) = c;   % ICs
+
+% ode - manually step through using Forward Euler
+for idx = 1:length(t)-1
+    % if reaches max spike potential
+    if v(idx) >= maxSpike
+        % fprintf('Max spike reached at t=%.2f\n',idx*dt)
+        v(idx) = maxSpike; % makes sure spikes don't exceed this
+        v(idx+1) = c;
+        u(idx+1) = u(idx) + d;
+    else      
+        % ODE: next = current + dt * dy/dt
+        u(idx+1) = u(idx) + dt * (a * (b*v(idx) - u(idx)));
+        v(idx+1) = v(idx) + dt * (A*v(idx)^2 + B*v(idx) + C - u(idx) + I(idx));
+    end
 end
 
-% get input value at this time
-I_t = interp1(tspan,I,t);
+% plot time repsonse vs. input
+figure;
 
-dydt(1) = 0.04*v^2 + 5*v + 140 - u + I_t;
-dydt(2) = a * (b*v - u); 
+ax1 = subplot(2,1,1);
+plot(t,v)
+title('Voltage time response of Izhikevich simple model')
+xlabel('Time (ms)')
+ylabel('Voltage (mV)')
+
+ax2 = subplot(2,1,2);
+plot(t,I)
+title('Input function')
+xlabel('Time (ms)')
+ylabel('Voltage (mV)')
+
+linkaxes([ax1 ax2],'xy')
 
 end
