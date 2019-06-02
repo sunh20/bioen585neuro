f5b706f60f2bb7207fd12fc4c24bbad3183d1ffe
Meriam Lahrichi <50497798+merilah@users.noreply.github.com>
2019-06-02 13:46:47 -0700

    neuron_withPSP
    Added PSP behavior to neuron class

---
+++ code/neuron_withPSP.m
@@ -0,0 +1,81 @@
+classdef neuron < handle
+    properties
+        resting; % Resting membrane potential
+        intra; % Log of intracellular potentials
+        output; % Output voltage from synapse
+        maxSpike; % Maximum voltage of synapse spike
+        eqnParams; % Izhikevich model
+        sens; % Log of sensitivity values
+        inhib; % Marks if neuron is inhibitory
+        spiking; % Marks current time in spike
+        name; % Name of neuron
+        spikeLog; % Tracks when spikes occur
+    end
+    
+    methods
+        function obj = neuron()
+            obj.resting = -65;
+            obj.intra = [obj.resting]; 
+            obj.output = 30; 
+            obj.maxSpike = 30; 
+            obj.eqnParams = [0.04 5 140 0.02 0.2 obj.resting 8 30];
+            obj.sens = [obj.eqnParams(7)];
+            obj.inhib = false; 
+            obj.spiking = 0; 
+            obj.spikeLog = [];
+        end
+        
+        function spike = addPotential(obj, stimulus, dt)
+            % Adds the provided stimulus to the neuron's potential. Note
+            % that dt corresponds to the time between steps. 
+            % If the neuron synapses (i.e. threshold is exceeded), causes
+            % neuron to output a post synaptic potential.
+            
+            spike = 0; % Output of neuron. Modify this for output of neuron
+            
+            % Runs if neuron is currently spiking. Outputs post synaptic
+            % potential if neuron is spiking. If spiking has ran for over
+            % 500 time steps, stops spiking.
+            
+            if obj.spiking > 0
+                if obj.inhib == true
+                    spike = genPSP(2, 0, 1)
+                    spike = obj.output;
+                else
+                    spike = genPSP(2, 1, 1)
+                    spike = obj.output; 
+                obj.spiking = obj.spiking + 1;
+                if obj.spiking > 500
+                    obj.spiking = 0;
+                end
+                end
+            end
+            
+            % Runs Izhikevich. Adds current intracellular potential to
+            % neuron log. If neuron exceeds maxSpike, begins spiking and
+            % signals start of post synaptic potential.
+            if obj.intra(end) >= obj.maxSpike && obj.spiking == 0
+                obj.intra(end) = obj.maxSpike; % makes sure spikes don't exceed this
+                obj.intra(end+1) = obj.resting;
+                obj.sens(end+1) = obj.sens(end) + obj.eqnParams(8);
+                obj.spiking = 1;
+                obj.spikeLog(end + 1) = 1;
+            else 
+                obj.sens(end+1) = obj.sens(end) + dt * (obj.eqnParams(4)... 
+                                  * (obj.eqnParams(5) * obj.intra(end)...
+                                  - obj.sens(end)));
+                obj.intra(end+1) = obj.intra(end) + dt * (obj.eqnParams(1)...
+                                   * obj.intra(end)^2 + obj.eqnParams(2)...
+                                   * obj.intra(end) + obj.eqnParams(3)...
+                                   - obj.sens(end) + stimulus);
+               obj.spikeLog(end + 1) = 0;
+            end            
+        end
+        
+        function setPotential(obj, potential)
+            % Sets the potential of the neuron to the provided value.
+            obj.intra = potential;
+        end
+        
+    end
+end
